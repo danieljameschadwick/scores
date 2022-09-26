ea45f0bf1b5f1271afa445c221082cc4f305ac76
Daniel Chadwick <djchadwick@outlook.com>
2022-09-26 21:19:56 +0100

    chore: @scores/expo (SettingsScreen: updated dark mode setting)

--- services/expo/src/screens/SettingsScreen.tsx
+++ services/expo/src/screens/SettingsScreen.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, Switch } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import IonIcon from "react-native-vector-icons/Ionicons";
 import { useAppDispatch, useAppSelector } from "@scores/state/hooks";
 import { selectTheme, setTheme } from "@scores/state/reducer/ThemeReducer";
 import { Theme } from "@scores/types/enum/Theme";
@@ -21,37 +20,45 @@ export const SettingsScreen: React.FC<Props> = ({ navigation }) => {
     container: {
       height: "100%",
       width: "100%",
+      paddingTop: 15,
       paddingBottom: insets.bottom,
       paddingHorizontal: 15,
     },
     row: {
       display: "flex",
       flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      paddingVertical: 10,
+      paddingHorizontal: 15,
+      borderRadius: 5,
     },
     text: {
+      fontSize: 16,
       marginRight: 5,
     },
   });
 
-  const dispatchTheme = (theme: Theme) => {
+  const dispatchTheme = (isDarkMode: boolean) => {
+    const theme = isDarkMode ? Theme.DARK_MODE : Theme.LIGHT_MODE;
+
     dispatch(setTheme(theme));
   };
 
   return (
-    <View style={[styles.container, themeStyles.container]}>
-      <View style={[styles.row]}>
-        <Text style={[styles.text, themeStyles.text]}>Theme:</Text>
+    <View style={[styles.container, themeStyles.lightContainer]}>
+      <View style={[styles.row, themeStyles.container]}>
+        <Text style={[styles.text, themeStyles.text]}>Dark Mode</Text>
 
-        {theme === Theme.LIGHT_MODE && (
-          <TouchableOpacity onPress={() => dispatchTheme(Theme.DARK_MODE)}>
-            <IonIcon style={[themeStyles.text]} name={"sunny"} />
-          </TouchableOpacity>
-        )}
-        {theme === Theme.DARK_MODE && (
-          <TouchableOpacity onPress={() => dispatchTheme(Theme.LIGHT_MODE)}>
-            <IonIcon style={[themeStyles.text]} name={"moon"} />
-          </TouchableOpacity>
-        )}
+        <View>
+          <Switch
+            trackColor={{ false: "#FFFFFF", true: "green" }}
+            thumbColor={"#FFFFFF"}
+            ios_backgroundColor={"white"}
+            onValueChange={dispatchTheme}
+            value={theme === Theme.DARK_MODE}
+          />
+        </View>
       </View>
     </View>
   );
